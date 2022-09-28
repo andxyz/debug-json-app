460ad74b9d248a393343a0240cb1be84e3333066
Andrew Stevens <andxyz.stevens@gmail.com>
2022-09-28 11:31:28 -0400

    Rubocop config files

--- unicorn_conf.rb
+++ unicorn_conf.rb
@@ -1,7 +1,9 @@
-if ENV["RACK_ENV"] == "development"
+# frozen_string_literal: true
+
+if ENV['RACK_ENV'] == 'development'
   APP_ROOT = Dir.getwd
   working_directory APP_ROOT
-  stderr_path APP_ROOT + "/log/unicorn.stderr.log"
+  stderr_path "#{APP_ROOT}/log/unicorn.stderr.log"
 
   OPTIONS_HASH = {
     # ipv6only: false,
@@ -11,14 +13,14 @@ if ENV["RACK_ENV"] == "development"
   }.freeze
 
   # see https://github.com/defunkt/unicorn/blob/v5.2.0/lib/unicorn/configurator.rb#L407
-  listen "localhost:4567", OPTIONS_HASH
-  listen "127.0.0.1:4567", OPTIONS_HASH # send some data here
-  listen "localhost:9090", OPTIONS_HASH
-  listen "127.0.0.1:9090", OPTIONS_HASH # or send some data here
-  listen "localhost:9091", OPTIONS_HASH
-  listen "127.0.0.1:9091", OPTIONS_HASH # send some data here
-  listen "localhost:8125", OPTIONS_HASH
-  listen "127.0.0.1:8125", OPTIONS_HASH # or send some data here
+  listen 'localhost:4567', OPTIONS_HASH
+  listen '127.0.0.1:4567', OPTIONS_HASH # send some data here
+  listen 'localhost:9090', OPTIONS_HASH
+  listen '127.0.0.1:9090', OPTIONS_HASH # or send some data here
+  listen 'localhost:9091', OPTIONS_HASH
+  listen '127.0.0.1:9091', OPTIONS_HASH # send some data here
+  listen 'localhost:8125', OPTIONS_HASH
+  listen '127.0.0.1:8125', OPTIONS_HASH # or send some data here
   # listen '/tmp/http_logger_app.unicorn.sock', *OPTIONS
 
   worker_processes 1
